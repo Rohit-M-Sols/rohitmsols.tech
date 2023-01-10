45d4e0d892f5bdd4409203a41438c6b916dadb2d
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-01-11 00:02:45 +0530

    Updates

--- main.js
+++ main.js
@@ -3,6 +3,6 @@
 var __webpack_exports__ = {};
 
 
-alert('The website is not complete yet! Current deployment is for testing purposes only.');
+// alert('The website is not complete yet! Current deployment is for testing purposes only.');
 /******/ })()
 ;
